71805053d3b55a1e463f6568868ac7a9ac8764eb
littleghost <785340571@qq.com>
2021-04-10 17:17:22 +0800

    更新：对传入*[]int的原因说明

--- 144.二叉树的前序遍历/main.go
+++ 144.二叉树的前序遍历/main.go
@@ -12,6 +12,9 @@ func preorderTraversal(root *TreeNode) []int {
 	return result
 }
 
+// 这里为什么要传*[]int而不是[]int。
+// 因为preorder函数里面有append操作，有对[]int的len和cap函数操作的地方，所以要传地址
+// 如果只有值的修改和读取（即不对len和cap进行修改则可以改用直接传[]int
 func preorder(root *TreeNode, result *[]int) {
 	if root == nil {
 		return
